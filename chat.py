381a6e912268048b58ef6ecabfdec33cb1ead160
Devin Berchtold <devinberchtold@gmail.com>
2023-03-28 20:15:14 -0400

    Autosaving, YAML, improved commands

--- chat.py
+++ chat.py
@@ -19,6 +19,11 @@ import tiktoken
 ##    ## ##     ## ##   ### ##    ##    ##    ##     ## ##   ###    ##    ##    ##
  ######   #######  ##    ##  ######     ##    ##     ## ##    ##    ##     ######
 
+YAML = True
+if YAML:
+    import yaml
+JSON = True
+
 DEBUG = False
 MAX_TOKENS = 4000
 DIALOGUE_TRIES = 3
@@ -33,33 +38,44 @@ TRANSLATE = False
 USER_LANG = 'English'
 AI_LANG = 'Chinese'
 
+AUTOSAVE = True
+AUTOLOAD = True
+AUTOLOAD_DEFAULT = 'help'
+FILENAME = datetime.now().strftime('%Y-%m-%d')
+
 # MODEL = "gpt-3.5-turbo" # Cheaper
 MODEL = "gpt-4" # Better
 STREAM = True
-USERNAME = os.getlogin()
+AI_NAME = 'Red'
+USER_NAME = 'Blue'
+try:
+    USER_ID = os.getlogin()
+except Exception:
+    USER_ID = 'unknown'
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Describes the world the story is set in plus more specific details of the room the character is in. Used for AI description and included at the beginning of every chat for context
 setting = "It's a slow afternoon at the pet store."
 
 # Describe the character from the character's perspective (The character is 'you')
-red_character = "You are Red, an old, grumpy, widowed man. You're here to buy food for your late wife's cat that you're taking care of. You will never adopt a dog. You would be shocked to learn a dog could speak."
+red_character = f"You are {AI_NAME}, an old, grumpy, widowed man. You're here to buy food for your late wife's cat that you're taking care of. You will never adopt a dog. You would be shocked to learn a dog could speak."
 # that only speaks in haiku's. You get embarrassed if your haiku has the wrong number of syllables
 
-# Describe what the character should know about the blue (the player). From the characters perspective (Blue is 'he/she/they')
-blue_character = "They are Blue, a cute little dog sitting in the middle of their square, gray cell. There is an enclosure with towels in the back for them to sleep in private."
+# Describe what the character should know about the user. From the characters perspective (User is 'he/she/they')
+blue_character = f"They are {USER_NAME}, a cute little dog sitting in the middle of their square, gray cell. There is an enclosure with towels in the back for them to sleep in private."
 
-# How the character knows the player? What caused this conversation to start? Does Red have a goal related to the player?
+# How the character knows the user? What caused this conversation to start? Does the AI have a goal related to the user?
 red_to_blue = "You walk down the aisle of the pet store, towards the back you notice a sign that reads 'ADOPTIONS'. You see a little dog."
 
 # Use this to ask the AI to describe the scene
 blue_perspective = "You sit in the middle of your square, gray kennel. There is an enclosure with towels in the back for you to sleep in private."
 
-response_directive = "Give the response like this:\n(<adjective describing Red's tone>) <what Red said>"
+response_directive = f"Give the response like this:\n(<adjective describing {AI_NAME}'s tone>) <what {AI_NAME} said>"
 
-system_directive = {"role": "system", "content": f"{setting} {red_character} {red_to_blue} {blue_character} What does Red say? {response_directive}"}
+system_directive = {"role": "system", "content": f"{setting} {red_character} {red_to_blue} {blue_character} What does {AI_NAME} say? {response_directive}"}
 
-system_backup = {"role": "system", "content": f"{setting} {red_character} {red_to_blue} {blue_character} What does Red say?"}
+system_backup = {"role": "system", "content": f"{setting} {red_character} {red_to_blue} {blue_character} What does {AI_NAME} say?"}
 
 messages = [
     system_directive
@@ -78,6 +94,13 @@ history = []
 ##       ##     ## ##   ### ##    ##    ##     ##  ##     ## ##   ### ##    ##
 ##        #######  ##    ##  ######     ##    ####  #######  ##    ##  ######
 
+def remove_spaces(string):
+    lines = string.split('\n') # split into lines
+    lines = [l.rstrip() for l in lines] # remove trailing spaces
+    return '\n'.join(lines) # join and return
+
+encoding = tiktoken.encoding_for_model(MODEL)
+
 def chat_complete(messages, model=MODEL, temperature=0.8, prefix='', print_result=True):
     for _ in range(5):
         try:
@@ -86,7 +109,7 @@ def chat_complete(messages, model=MODEL, temperature=0.8, prefix='', print_resul
                 model=model,
                 messages=messages,
                 temperature=temperature,
-                user=USERNAME,
+                user=USER_ID,
                 stream=(STREAM and print_result)
             )
             full_string = ''
@@ -110,10 +133,11 @@ def chat_complete(messages, model=MODEL, temperature=0.8, prefix='', print_resul
                 total_time = time.time()-t0
                 if DEBUG: # print debug info
                     chunks = len(collected_chunks)
+                    tokens = len(encoding.encode(full_string))
                     finish = chunk['choices'][0]['finish_reason']
-                    print(f'<chat {chunks=}, {finish=}, time={total_time:.3}s>')
+                    print(f'<chat {chunks=}, {tokens=}, {finish=}, time={total_time:.3}s>')
 
-                return full_string
+                return remove_spaces(full_string) # Remove trailing spaces and return
 
             else:
                 full_string = response['choices'][0]['message']['content']
@@ -128,8 +152,9 @@ def chat_complete(messages, model=MODEL, temperature=0.8, prefix='', print_resul
                     total = response['usage']['total_tokens']
                     finish = response['choices'][0]['finish_reason']
                     print(f'<chat tokens=({prompt}, {completion}, {total}), {finish=}, time={total_time:.3}s>')
-    
-            return full_string
+
+
+                return remove_spaces(full_string) # Remove trailing spaces and return
 
         except Exception as e:
             print(f"Error: {e} Trying again in 1 second...")
@@ -153,9 +178,9 @@ def messages_string(messages_to_summarize):
             # string += f' System: {s}'
             string += f' {s}'
         if m['role'] == 'user':
-            string += f' Blue: {s}'
+            string += f' {USER_NAME}: {s}'
         if m['role'] == 'assistant':
-            string += f' Red: {s}'
+            string += f' {AI_NAME}: {s}'
 
     return string.strip()
 
@@ -177,18 +202,18 @@ def get_summary(request):
     return answer
 
 def get_dialogue(user_input):
-    messages.append( {"role": "user", "content": user_input} )
-    history.append( {"role": "user", "content": user_input} )
+    message = {"role": "user", "content": user_input}
+    messages.append(message)
+    history.append(message)
 
     answer = ''
-    time = 0
 
     for n in range(DIALOGUE_TRIES):
         if n == DIALOGUE_TRIES-1:
             messages[0] = system_backup
             print("<Parenthesis failed! Moving to backup>")
             
-        answer = chat_complete(messages, prefix=f"\n({len(history)+1}) Red: ")
+        answer = chat_complete(messages, prefix=f"\n({len(history)+1}) {AI_NAME}: ")
 
         # check for match
         if VALIDATE_FORMAT:
@@ -207,8 +232,9 @@ def get_dialogue(user_input):
             print('<OpenAI gave unexpected result>')
             print(answer)
 
-    messages.append( { "role": "assistant", "content": answer } )
-    history.append( { "role": "assistant", "content": answer } )
+    message = { "role": "assistant", "content": answer }
+    messages.append(message)
+    history.append(message)
     if VALIDATE_FORMAT:
         messages[0] = system_directive
 
@@ -234,37 +260,68 @@ def summarize(n=5000, delete=False):
 
     return get_summary(string)
 
-ENCODING = tiktoken.encoding_for_model(MODEL)
 def num_tokens_from_messages(messages):
     """Returns the number of tokens used by a list of messages. Copied from OpenAI example"""
     num_tokens = 0
     for message in messages:
         num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
         for key, value in message.items():
-            num_tokens += len(ENCODING.encode(value))
+            num_tokens += len(encoding.encode(value))
             if key == "name":  # if there's a name, the role is omitted
                 num_tokens += -1  # role is always required and always 1 token
     num_tokens += 2  # every reply is primed with <im_start>assistant
     return num_tokens
 
-def file(user_input):
-    if re.match(r"!\w*$", user_input): # Just '!command', no filename
-        filename = input('Filename: ') # Ask for filename
-    else: # '!command filename'
-        filename = user_input.split(' ')[1] # Filename is after space
+if YAML: # YAML copy for readability. Only define functions if needed
+    def str_presenter(dumper, data):
+        s = '|' if '\n' in data else None # Change style to | if multiple lines
+        # print(f'{s} {data[:10]}')
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style=s)
 
-    if filename.endswith('.json'): # Remove .json if specified
-        filename = filename.split('.')[0]
+    def dict_presenter(dumper, data):
+        f = all([s.isupper() for s in data.keys()]) # True if all keys in data are uppercase
+        return dumper.represent_mapping('tag:yaml.org,2002:map', data, flow_style=f)
 
-    return filename
+    yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(dict, dict_presenter)
 
 def load(filename):
     global messages, history
-    messages = json.load(open(f'{filename}.json'))
-    if os.path.isfile(f'{filename}_h.json'):
-        history = json.load(open(f'{filename}_h.json'))
+    messages, history = [], []
+    if os.path.isfile(f'conversations/{filename}.json'):
+        print(f"Loading {filename}.json")
+        data = json.load(open(f'conversations/{filename}.json', encoding='utf8'))
+    elif YAML:
+        if os.path.isfile(f'conversations/{filename}.yaml'):
+            print(f"Loading {filename}.yaml")
+            data = yaml.safe_load(open(f'conversations/{filename}.yaml', 'r', encoding='utf8'))
+        else:
+            return False
     else:
+        return False
+    
+    # We loaded a file, extract the data
+    if isinstance(data, dict):
+        for k, v in data.items():
+            if k == 'global_vars':
+                for x, y in v.items(): # for each GLOBAL VAR
+                    if x not in ['DEBUG', 'USER_ID']: # Don't restore these
+                        globals()[x] = y
+            else: # messages, history
+                globals()[k] = v
+    elif isinstance(data, list): # List of messages format
+        messages = data
+    else:
+        print('Unknown Data')
+    
+    if history == []: # No history? Copy from messages
         history = [m for m in messages if m['role'] != 'system']
+
+    else: # We have history. Connect to messages so they are in sync
+        n = 1 # Count the number of identical messages
+        while (history[-n] == messages[-n]) and (n < len(history)):
+            n += 1
+        history[-n+1:] = messages[-n+1:] # Reassign so they are the same object
 
     if messages[-1]['role'] == 'user': # If last response is user...
         messages.pop() # Remove it so user can respond
@@ -272,8 +329,31 @@ def load(filename):
 
     if len(messages) > 1:
         if messages[1]['role'] == 'system': # Second messages is system...
+            global SUMMARIZE, SUMMARIZED # This means it was summarized
             SUMMARIZE = True
             SUMMARIZED = True
+
+        # Print last messages from loaded file
+        # print(f"\n(0) {messages[0]['content']}") # Print system message
+        if len(messages) > 2:
+            l = len(history) # Print last question and answer
+            print(f"\n({l-1}) {USER_NAME}: {messages[-2]['content']}")
+            print(f"\n({l}) {AI_NAME}: {messages[-1]['content']}")
+
+    return True
+
+def save(filename):
+    if not os.path.exists("conversations"):
+        os.makedirs('conversations')
+
+    # All (uppercase) global variables minus built-in variables (__)
+    global_vars = {k: v for k, v in globals().items() if not k.startswith("__") and k.isupper()}
+    data = {'global_vars': global_vars, 'messages': messages, 'history': history}
+
+    if YAML:
+        yaml.safe_dump(data, open(f'conversations/{filename}.yaml', 'w', encoding='utf8'), allow_unicode=True, sort_keys=False, width=float("inf"))
+    if JSON:
+        json.dump(data, open(f'conversations/{filename}.json', 'w', encoding='utf8'), indent='\t', ensure_ascii=False)
 
 
 
@@ -289,112 +369,125 @@ def load(filename):
 if len(sys.argv) == 2: # If there's an argument...
     # Load an existing conversation
     arg = sys.argv[1]
-    if arg.endswith('.json'):
+    if arg.endswith('.json') or arg.endswith('.yaml'):
         arg = arg.split('.')[0]
-    print(f'Loading {arg}.json')
     load(arg)
-    # Print last message from loaded file
-    print(f"\n(0) {messages[0]['content']}") # Print system message
-    if len(messages) > 2:
-        l = len(history)
-        print(f"\n({l-1}) {messages[-2]['content']}")
-        print(f"\n({l}) {messages[-1]['content']}")
 
-else: # Setup a new conversation
+elif AUTOLOAD:
+    filename = datetime.now().strftime('%Y-%m-%d')
+    if not load(filename):
+        load(AUTOLOAD_DEFAULT)
+
+else: # Setup a new conversation (Blue)
     print("You wake up in your kennel at the back of the pet store.")
-
     print(get_response(f"{setting} {blue_perspective} Write a couple sentences to describe the scene"))
-
     print("You see an old man.")
 
-try:
-    while True:
-        # Get a new input
-        user_input = input(f'\n({len(history)+1}) Blue: ').strip()
-        # print('')
+# try:
+while True:
+    # Get a new input
+    user_input = remove_spaces(input(f'\n({len(history)+1}) {USER_NAME}: '))
+
+    if user_input.startswith('!'):
+        input_split = user_input.split(' ')
+        command = input_split[0]
+        if len(input_split) > 1:
+            parm = input_split[1]
+        else:
+            parm = ''
 
         # Process commands
-        if user_input == '!exit':
+        if command == '!exit':
             print('Goodbye')
             break
 
-        if user_input == '!restart':
+        elif command == '!restart':
             print('Restarting conversation')
             messages = [messages[0]]
             history = []
-            continue
 
-        if user_input == '!redo':
+        elif command == '!redo' or command == '!undo':
             if len(messages) > 1:
                 print('Removing last 2 messages')
                 messages = messages[:-2]
                 history = history[:-2]
             else:
                 print('No messages to remove')
-            continue
 
-        if user_input == '!summary':
-            print(f'<Summary: {summarize()}>')
-            continue
+        elif command == '!summary':
+            if parm:
+                print(f'<Summary: {summarize(int(parm))}>')
+            else:
+                print(f'<Summary: {summarize()}>')
 
-        if user_input == '!print' or user_input == '!messages':
+        elif command == '!print' or command == '!messages':
             for m in messages:
                 print(m['role']+': '+m['content'])
-            continue
 
-        if user_input == '!history':
+        elif command == '!history':
             for m in history:
                 print(m['role']+': '+m['content'])
-            continue
 
-        if user_input.startswith('!save'):
-            filename = file(user_input)
-            json.dump(messages, open(f'{filename}.json', 'w'), indent='\t')
-            json.dump(history, open(f'{filename}_h.json', 'w'), indent='\t')
-            continue
+        elif command == '!save':
+            if parm:
+                save(parm)
+                print(f"Data saved to {parm}")
+            else:
+                save(FILENAME)
+                print(f"Data saved to {FILENAME}")
 
-        if user_input.startswith('!load'):
-            filename = file(user_input)
-            load(filename)
-            continue
+        elif command == '!load':
+            if parm:
+                load(parm)
+            else:
+                load(FILENAME)
 
-        if user_input.startswith('!translate'):
+        elif command == '!model':
+            if parm:
+                MODEL = parm
+            print(f"<Model: {MODEL}>")
+
+        elif command == '!translate':
             if user_input == '!translate':
                 TRANSLATE = not TRANSLATE
             else:
-                split = user_input.split(' ')
-                if len(split) == 2:
+                if len(input_split) == 2:
                     TRANSLATE = True
-                    AI_LANG = split[1]
+                    AI_LANG = parm
                     USER_LANG = 'English'
-                elif len(split) == 3:
+                elif len(input_split) == 3:
                     TRANSLATE = True
-                    AI_LANG = split[1]
-                    USER_LANG = split[2]
+                    AI_LANG = parm
+                    USER_LANG = input_split[2]
 
             print(f'<Translate={TRANSLATE}, AI={AI_LANG}, User={USER_LANG}>')
-            continue
 
-        if user_input == '!debug':
+        elif command == '!debug':
             DEBUG = not DEBUG
-            print(f'{DEBUG=}')
-            continue
-
+            print(f'<{DEBUG=}>')
+        
+    else: # Doesn't start with '!'
         if TRANSLATE:
             user_input = get_translation(user_input, USER_LANG, AI_LANG)
 
         # This isn't a command, process dialogue
         answer = get_dialogue(user_input)
         while not answer:
-            user_input = input(f'\n({len(history)}) Blue: ').strip()
+            user_input = remove_spaces(input(f'\n({len(history)}) {USER_NAME}: '))
             answer = get_dialogue(user_input)
-        
+
         if TRANSLATE:
             answer = get_translation(answer, AI_LANG, USER_LANG)
 
+        if AUTOSAVE:
+            save(FILENAME)
+
         # Summarize or forget if approaching token limit
         if SUMMARIZE:
-            if num_tokens_from_messages(messages) > MAX_TOKENS:
+            total_tokens = num_tokens_from_messages(messages)
+            if DEBUG:
+                print(f'<{total_tokens=}>')
+            if total_tokens > MAX_TOKENS:
                 summary = summarize(SUMMARIZE_LEN, True)
                 if DEBUG: print(f'<Summary: {summary}>')
 
@@ -412,9 +505,7 @@ try:
                 if DEBUG: print(f'<Forgetting: {last}>')
                 del messages[1]
 
-except Exception as e: # Unexpected error. Dump all data
-    print(e)
-    time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    json.dump(messages, open(f'crash_{time}.json', 'w'), indent='\t')
-    json.dump(history, open(f'crash_{time}_h.json', 'w'), indent='\t')
-    print(f'Data saved to crash_{time}.json')
+# except Exception as e: # Unexpected error. Dump all data
+#     print(e)
+#     time_string = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+#     save(f"crash_{time_string}")
